39e0090cddb5e790d9060c1cd3f72f67a59b2d1a
Michał Dwojak <michal.dwojak92@hotmail.com>
2017-04-10 16:22:12 +0200

    Add - design and method async

--- MassEmail/Form1.Designer.cs
+++ MassEmail/Form1.Designer.cs
@@ -33,7 +33,26 @@
             this.listBoxAdresy = new System.Windows.Forms.ListBox();
             this.buttonDodajAdres = new System.Windows.Forms.Button();
             this.textBoxAdres = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBoxSmtp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.maskedTextBoxPassword = new System.Windows.Forms.MaskedTextBox();
+            this.textBoxTemat = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxNadawca = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listBoxZalaczniki = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxTresc = new System.Windows.Forms.TextBox();
+            this.buttonWyslij = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -55,6 +74,7 @@
             this.listBoxAdresy.Name = "listBoxAdresy";
             this.listBoxAdresy.Size = new System.Drawing.Size(208, 420);
             this.listBoxAdresy.TabIndex = 0;
+            this.listBoxAdresy.DoubleClick += new System.EventHandler(this.listBoxAdresy_DoubleClick);
             // 
             // buttonDodajAdres
             // 
@@ -72,12 +92,180 @@
             this.textBoxAdres.Name = "textBoxAdres";
             this.textBoxAdres.Size = new System.Drawing.Size(208, 20);
             this.textBoxAdres.TabIndex = 2;
+            this.textBoxAdres.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxAdres_KeyDown);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonWyslij);
+            this.groupBox2.Controls.Add(this.textBoxTresc);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.listBoxZalaczniki);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.textBoxNadawca);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.textBoxTemat);
+            this.groupBox2.Controls.Add(this.maskedTextBoxPassword);
+            this.groupBox2.Controls.Add(this.textBoxLogin);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.textBoxSmtp);
+            this.groupBox2.Location = new System.Drawing.Point(239, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(278, 505);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Dane do e-maila:";
+            // 
+            // textBoxSmtp
+            // 
+            this.textBoxSmtp.Location = new System.Drawing.Point(88, 19);
+            this.textBoxSmtp.Name = "textBoxSmtp";
+            this.textBoxSmtp.Size = new System.Drawing.Size(184, 20);
+            this.textBoxSmtp.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Serwer SMTP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Login:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Hasło:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Temat:";
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(88, 44);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(184, 20);
+            this.textBoxLogin.TabIndex = 5;
+            // 
+            // maskedTextBoxPassword
+            // 
+            this.maskedTextBoxPassword.Location = new System.Drawing.Point(88, 70);
+            this.maskedTextBoxPassword.Name = "maskedTextBoxPassword";
+            this.maskedTextBoxPassword.PasswordChar = '#';
+            this.maskedTextBoxPassword.Size = new System.Drawing.Size(184, 20);
+            this.maskedTextBoxPassword.TabIndex = 6;
+            // 
+            // textBoxTemat
+            // 
+            this.textBoxTemat.Location = new System.Drawing.Point(9, 168);
+            this.textBoxTemat.Name = "textBoxTemat";
+            this.textBoxTemat.Size = new System.Drawing.Size(263, 20);
+            this.textBoxTemat.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 202);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Załączniki:";
+            // 
+            // textBoxNadawca
+            // 
+            this.textBoxNadawca.Location = new System.Drawing.Point(88, 97);
+            this.textBoxNadawca.Name = "textBoxNadawca";
+            this.textBoxNadawca.Size = new System.Drawing.Size(184, 20);
+            this.textBoxNadawca.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Nadawca:";
+            // 
+            // listBoxZalaczniki
+            // 
+            this.listBoxZalaczniki.FormattingEnabled = true;
+            this.listBoxZalaczniki.HorizontalScrollbar = true;
+            this.listBoxZalaczniki.Location = new System.Drawing.Point(9, 219);
+            this.listBoxZalaczniki.Name = "listBoxZalaczniki";
+            this.listBoxZalaczniki.Size = new System.Drawing.Size(263, 82);
+            this.listBoxZalaczniki.TabIndex = 11;
+            this.listBoxZalaczniki.DoubleClick += new System.EventHandler(this.listBoxZalaczniki_DoubleClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(233, 307);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(39, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "[...]";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(7, 329);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Treść:";
+            // 
+            // textBoxTresc
+            // 
+            this.textBoxTresc.Location = new System.Drawing.Point(9, 346);
+            this.textBoxTresc.Multiline = true;
+            this.textBoxTresc.Name = "textBoxTresc";
+            this.textBoxTresc.Size = new System.Drawing.Size(263, 119);
+            this.textBoxTresc.TabIndex = 14;
+            // 
+            // buttonWyslij
+            // 
+            this.buttonWyslij.Location = new System.Drawing.Point(100, 471);
+            this.buttonWyslij.Name = "buttonWyslij";
+            this.buttonWyslij.Size = new System.Drawing.Size(75, 23);
+            this.buttonWyslij.TabIndex = 15;
+            this.buttonWyslij.Text = "Wyślij";
+            this.buttonWyslij.UseVisualStyleBackColor = true;
+            this.buttonWyslij.Click += new System.EventHandler(this.buttonWyslij_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(529, 525);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -86,6 +274,8 @@
             this.Text = "MD Masowa Poczta";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -96,6 +286,24 @@
         private System.Windows.Forms.ListBox listBoxAdresy;
         private System.Windows.Forms.TextBox textBoxAdres;
         private System.Windows.Forms.Button buttonDodajAdres;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBoxZalaczniki;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxNadawca;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxTemat;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxPassword;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxSmtp;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonWyslij;
+        private System.Windows.Forms.TextBox textBoxTresc;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- MassEmail/Form1.cs
+++ MassEmail/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
 
 namespace MassEmail
 {
@@ -16,6 +18,42 @@ namespace MassEmail
         {
             InitializeComponent();
         }
+
+        private void WyslijAsynchr(MailMessage wiadomosc)
+        {
+            try
+            {
+                SmtpClient klient = new SmtpClient(textBoxSmtp.Text);
+                if (textBoxLogin.Text != String.Empty && maskedTextBoxPassword.Text != String.Empty)
+                    klient.Credentials = new NetworkCredential(textBoxLogin.Text, maskedTextBoxPassword.Text);
+                else
+                {
+                    MessageBox.Show("Proszę podać nazwę użytkownika i hasło");
+                    return;
+                }
+                klient.EnableSsl = true;
+                klient.SendCompleted += new SendCompletedEventHandler(Wyslane);
+                klient.SendAsync(wiadomosc, null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Błąd: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Wyslane(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Cancelled | e.Error != null)
+            {
+                MessageBox.Show("Błąd: Wysyłanie anulowane bądź wystąpił błąd serwera");
+                
+            }
+            else
+            {
+                MessageBox.Show("Wiadomość wysłana");
+            }
+        }
+
 
         private void buttonDodajAdres_Click(object sender, EventArgs e)
         {
@@ -32,6 +70,80 @@ namespace MassEmail
             }
           
 
+        }
+
+        private void listBoxAdresy_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBoxAdresy.SelectedIndex != -1)
+                listBoxAdresy.Items.RemoveAt(listBoxAdresy.SelectedIndex);
+        }
+
+        private void textBoxAdres_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                buttonDodajAdres_Click(sender, e);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                listBoxZalaczniki.Items.Add(openFileDialog1.FileName);
+            }
+        }
+
+        private void listBoxZalaczniki_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBoxZalaczniki.SelectedIndex != -1)
+            {
+                listBoxZalaczniki.Items.RemoveAt(listBoxZalaczniki.SelectedIndex);
+            }
+        }
+
+        private void buttonWyslij_Click(object sender, EventArgs e)
+        {
+            MailAddress Od;
+            MailAddress Do;
+            MailMessage wiadomosc = new MailMessage();
+            try
+            {
+                Od = new MailAddress(textBoxNadawca.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Nieprawidłowy adres nadawcy");
+                textBoxNadawca.Clear();
+                return;
+            }
+            wiadomosc.From = Od;
+            if (listBoxZalaczniki.Items.Count > 0)
+                foreach (string plik in listBoxZalaczniki.Items)
+                {
+                    Attachment zalacznik = new Attachment(plik);
+                    wiadomosc.Attachments.Add(zalacznik);
+                }
+            try
+            {
+                foreach (string adres in listBoxAdresy.Items)
+                {
+                    Do = new MailAddress(adres);
+                    wiadomosc.To.Add(Do);
+                }
+                if (textBoxTemat.Text == String.Empty || textBoxTresc.Text == String.Empty)
+                {
+                    MessageBox.Show("Wprowadź treść wiadomości / temat","Błąd",MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+                wiadomosc.Subject = textBoxTemat.Text;
+                wiadomosc.Body = textBoxTresc.Text;
+                WyslijAsynchr(wiadomosc);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Błąd: " + ex.Message);
+            }
         }
     }
 }
